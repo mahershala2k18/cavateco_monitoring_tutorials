847fa200b2a229a93446cda0694fbda96ff13afc
David Chamussa <58297184+mahershala2k18@users.noreply.github.com>
2024-05-31 13:03:06 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Tutorial_01 from "./components/Tutorial_01";
+function App() {
+  return (
+    <>
+      <Tutorial_01 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Tutorial_01.jsx
@@ -0,0 +1,58 @@
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
+
+const Tutorial_01 = () => {
+  return (
+    <Box
+      className={"App-header"}
+      // sx={{
+      //   position: "static",
+      //   margin: "30px",
+      //   width: "896px",
+      //   height: "480px",
+      //   zIndex: "auto",
+      // }}
+    >
+      <Box>
+        <Box>
+          <Typography variant="h6">Tutorial Filtrar Equipas</Typography>
+        </Box>
+        <video width="750" height="500" controls>
+          <source src="/cavateco_monitoring_equipas.mp4" type="video/mp4" />
+        </video>
+      </Box>
+      <Box>
+        <Box>
+          <Typography variant="h6">Tutorial Fitrar Por datas</Typography>
+        </Box>
+        <video width="750" height="500" controls>
+          <source
+            src="/monitoring_encontros_date_filtering.mp4"
+            type="video/mp4"
+          />
+        </video>
+        {/* <Typography variant="caption">
+          Este tutorial ilustra como usar os filtros para obter dados segundo as
+          nossas escolhas de pesquisa.
+        </Typography> */}
+      </Box>
+      <Box>
+        <Box>
+          <Typography variant="h6">
+            Tutorial Uso de multiplos Filtros
+          </Typography>
+        </Box>
+        <video width="750" height="500" controls>
+          <source
+            src="/monitoring_encontros_multiple_filtering.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </Box>
+    </Box>
+  );
+};
+
+export default Tutorial_01;
